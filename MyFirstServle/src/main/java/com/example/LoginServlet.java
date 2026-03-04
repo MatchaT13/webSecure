f110dbe243e74977c3fefd34b0ae58de92c8416e
MatchaT13 <brian13@my.yorku.ca>
2026-03-03 20:12:00 -0500

    Add files via upload

--- MyFirstServle/src/main/java/com/example/LoginServlet.java
+++ MyFirstServle/src/main/java/com/example/LoginServlet.java
@@ -3,26 +3,72 @@ package com.example;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.io.File;
+
 
 public class LoginServlet extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException, IOException {
 
-        String user = request.getParameter("userid");
-        String pass = request.getParameter("password");
+	    String userInput = request.getParameter("userid");
+	    String passInput = request.getParameter("password");
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+	    boolean validUser = false;
 
-        if ("admin".equals(user) && "1234".equals(pass)) {
-            HttpSession session = request.getSession();
-            session.setAttribute("user", user);
-            out.println("<h2>Login Successful!</h2>");
-        } else {
-            out.println("<h2>Login Failed</h2>");
-        }
-    }
+	    try {
+
+	        // Get real path to users.xml inside WEB-INF
+	        String path = getServletContext().getRealPath("/WEB-INF/users.xml");
+	        File xmlFile = new File(path);
+
+	        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder builder = factory.newDocumentBuilder();
+	        Document doc = builder.parse(xmlFile);
+
+	        doc.getDocumentElement().normalize();
+
+	        NodeList userList = doc.getElementsByTagName("user");
+
+	        for (int i = 0; i < userList.getLength(); i++) {
+
+	            Node node = userList.item(i);
+
+	            if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+	                Element element = (Element) node;
+
+	                String userid = element.getElementsByTagName("userid")
+	                        .item(0).getTextContent();
+
+	                String password = element.getElementsByTagName("password")
+	                        .item(0).getTextContent();
+
+	                if (userid.equals(userInput) && password.equals(passInput)) {
+	                    validUser = true;
+	                    break;
+	                }
+	            }
+	        }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    if (validUser) {
+
+	        HttpSession session = request.getSession();
+	        session.setAttribute("username", userInput);
+
+	        response.sendRedirect("home.jsp");
+
+	    } else {
+
+	        response.sendRedirect("login.html");
+	    }
+	}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
